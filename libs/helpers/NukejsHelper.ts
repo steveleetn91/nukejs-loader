be6a600eddeccf6591ec7a759de110778c97c8be
Stevelee <hoang.le.tn91@gmail.com>
2023-04-07 00:06:26 +0700

    Prepare for beta 1.2

--- libs/helpers/NukejsHelper.ts
+++ libs/helpers/NukejsHelper.ts
@@ -7,7 +7,7 @@ export function isNukApp(data:string){
 export function allowsElements(){
     return ["SPAN","DIV","P","B","IMG","CANVAS","UL","LI","OL",
     "AUDIO","SOURCE","VIDEO","AUDIO","INPUT","BUTTON","LABEL","FORM","SELECT","OPTION","TEXTAREA","HR","IFRAME","NUKAPP",'BOX','A','H1','H2',
-    ,'H3','H4','H5','H6','CODE','SCRIPT',"TABLE","TR","TD","TH","THEAD","E","EM","BR"];
+    ,'H3','H4','H5','H6','CODE','SCRIPT',"TABLE","TR","TD","TH","THEAD","E","EM","BR","TBODY"];
 }
 export function createNukeId(){
     let id = () => {
